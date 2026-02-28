66363ae4763fac494d9d4dc5df9254e4ef5a5030
Kuro <85140724+kekosp@users.noreply.github.com>
2026-02-28 11:22:20 +0200

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.541b7c83680c47fe8dfc25833fe24b42',
-  appName: 'PVP Reports',
+  appId: 'app.lovable.k541b7c83680c47fe8dfc25833fe24b42',
+  appName: 'Reports',
   webDir: 'dist',
   plugins: {
     SplashScreen: {
